7246dac8dfc380261267900f08a3fc1a97bfe7fe
Politrees <143968312+Bebra777228@users.noreply.github.com>
2023-09-24 00:24:17 +0500

    Update igd.py

--- igd.py
+++ igd.py
@@ -6,7 +6,8 @@ import os
 if not os.path.exists('/content/drive'):
     print("Your drive is not mounted. Creating Fake Drive.")
     os.makedirs('/content/drive/MyDrive')
-wget "https://huggingface.co/Rejekts/project/resolve/main/project-main.zip" -O '/content/project-main.zip' && unzip -n 'project-main.zip' -d /content/drive/MyDrive
+link = https://huggingface.co/Rejekts/project/resolve/main/project-main.zip
+wget {link} -O '/content/project-main.zip' && unzip -n 'project-main.zip' -d /content/drive/MyDrive
 cd '/content/drive/MyDrive/project-main' && python download_files.py && pip install -r 'requirements-safe.txt'
 rm /content/project-main.zip
 rm -r /content/sample_data
