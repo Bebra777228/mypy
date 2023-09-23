bce6c1144920c37619d8f319b148f0cdb04eddfd
Politrees <143968312+Bebra777228@users.noreply.github.com>
2023-09-24 00:06:05 +0500

    Update igd.py

--- igd.py
+++ igd.py
@@ -11,9 +11,9 @@ import wget
 url = "https://huggingface.co/Rejekts/project/resolve/main/project-main.zip"
 file = wget.download(url,out="myFile.mp3")
 print(file)
-!cd '/content/drive/MyDrive/project-main' && python download_files.py && pip install -r 'requirements-safe.txt'
-!rm /content/project-main.zip
-!rm -r /content/sample_data
-!mkdir -p /content/dataset
+cd '/content/drive/MyDrive/project-main' && python download_files.py && pip install -r 'requirements-safe.txt'
+rm /content/project-main.zip
+rm -r /content/sample_data
+mkdir -p /content/dataset
 clear_output()
 Button(description="\u2714 Готово", button_style="success")
