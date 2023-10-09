111ca67dbf2946edb886249495faaef202c80265
Politrees <143968312+Bebra777228@users.noreply.github.com>
2023-10-10 01:41:34 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,14 +1,2 @@
 import os, sys
-with open(f'./logs/{model_name}/extract_f0_feature.log','w') as f:
-    print("Starting...")
-if f0method != "rmvpe_gpu":
-    os.system("python infer/modules/train/extract/extract_f0_print.py ./logs/{model_name} 2 {f0method}")
-else:
-    os.system("python infer/modules/train/extract/extract_f0_rmvpe.py 1 0 0 ./logs/{model_name} True")
-os.system("python infer/modules/train/extract_feature_print.py cuda:0 1 0 0 ./logs/{model_name} v2")
-with open(f'./logs/{model_name}/extract_f0_feature.log','r') as f:
-    if 'all-feature-done' in f.read():
-        clear_output()
-        display(Button(description="\u2714 Готово", button_style="success"))
-    else:
-        print("Ошибка предварительной обработки данных... Убедитесь, что данные были предварительно обработаны.")
+os.system("python infer/modules/train/extract/extract_f0_print.py ./logs/{model_name} 2 {f0method}")
