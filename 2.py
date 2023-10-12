1ae7b97fad8e3b1bd1aaa14aa94a34a3bc4616de
Politrees <143968312+Bebra777228@users.noreply.github.com>
2023-10-12 21:33:01 +0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,5 @@
 import os, sys
 os.system ("cd '/content/drive/MyDrive/project-main' && python download_files.py && pip install -r 'requirements-safe.txt'")
+os.system ("pip install pyngrok")
 os.system ("rm -r /content/sample_data")
 os.system ("mkdir -p /content/dataset")
