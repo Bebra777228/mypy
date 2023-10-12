1434fbef61c355bae990ae49e1a09577c380b6a1
Politrees <143968312+Bebra777228@users.noreply.github.com>
2023-10-12 21:32:10 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import os, sys
 os.system ("wget https://huggingface.co/Rejekts/project/resolve/main/project-main.zip -O '/content/project-main.zip' && unzip -n 'project-main.zip' -d /content/drive/MyDrive")
 os.system ("cd '/content/drive/MyDrive/project-main' && python download_files.py && pip install -r 'requirements-safe.txt'")
+os.system ("pip install pyngrok")
 os.system ("rm /content/project-main.zip")
 os.system ("rm -r /content/sample_data")
 os.system ("mkdir -p /content/dataset")
-
